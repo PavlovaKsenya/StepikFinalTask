143ced64ca3e88599e6eca18fe60e06401a3b4e6
PavlovaKsenya <snegurka64@mail.ru>
2022-03-22 16:35:50 +0400

    fix locators

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -12,7 +12,7 @@ class LoginPage(BasePage):
         assert "login" in self.browser.current_url, "Current url don't contain 'login'"
 
     def should_be_login_form(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "register form is not presented"
