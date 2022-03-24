2044c51e7e08145f7fd6099fa2f9af4e1cbcc45d
PavlovaKsenya <snegurka64@mail.ru>
2022-03-24 13:18:56 +0400

    fix login methods

--- pages/locators.py
+++ pages/locators.py
@@ -1,16 +1,17 @@
 from selenium.webdriver.common.by import By
 
-
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
 class LoginPageLocators():
     REGISTER_FORM = (By.ID, "register_form")
     LOGIN_FORM = (By.ID, "login_form")
 
 class ProductPageLocators():
-    ADD_TO_BUSKET = (By.CLASS_NAME, "btn-add-to-basket")
+    ADD_TO_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
     NAME_BOOK_ON_PAGE = (By.CSS_SELECTOR, ".product_main h1")
     NAME_BOOK_IN_BASKET = (By.CSS_SELECTOR, "#messages strong")
     PRICE_BOOK_ON_PAGE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRICE_BOOK_IN_BASKET = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alertinner")
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -4,7 +4,7 @@ from pages.locators import ProductPageLocators
 
 class ProductPage(BasePage):
     def add_to_busket(self):
-        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BUSKET)
+        link = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         link.click()
 
     def valid_names(self):
@@ -16,3 +16,11 @@ class ProductPage(BasePage):
         price1 = self.browser.find_element(*ProductPageLocators.PRICE_BOOK_ON_PAGE)
         price2 = self.browser.find_element(*ProductPageLocators.PRICE_BOOK_IN_BASKET)
         assert price1.text == price2.text, "WRONG PRICE OF BOOK"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
