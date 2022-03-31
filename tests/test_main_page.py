86000dbca521d4ecdc9e68dab31def88fca545c7
PavlovaKsenya <snegurka64@mail.ru>
2022-03-31 11:34:58 +0400

    add steps

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -10,7 +10,7 @@ from pages.login_page import LoginPage
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
     @allure.step
-    def test_guest_can_go_to_login_page(self, browser, screenshot_on_failure):
+    def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
         page.open()
@@ -19,14 +19,14 @@ class TestLoginFromMainPage():
         login_page.should_be_login_page()
 
     @allure.step
-    def test_guest_should_see_login_link(self, browser, screenshot_on_failure):
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
 @allure.step
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, screenshot_on_failure):
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -19,7 +19,7 @@ from pages.product_page import ProductPage
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 @allure.step
-def test_guest_can_add_product_to_basket(browser, link, screenshot_on_failure):
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_busket()
@@ -37,7 +37,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, 
     page.should_not_be_success_message()
 
 @allure.step
-def test_guest_cant_see_success_message(browser, screenshot_on_failure):
+def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -45,7 +45,7 @@ def test_guest_cant_see_success_message(browser, screenshot_on_failure):
 
 # @pytest.mark.xfail
 @allure.step
-def test_message_disappeared_after_adding_product_to_basket(browser, screenshot_on_failure):
+def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -53,7 +53,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser, screenshot_
     page.should_disappeared_success_message()
 
 @allure.step
-def test_guest_should_see_login_link_on_product_page(browser, screenshot_on_failure):
+def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
@@ -61,7 +61,7 @@ def test_guest_should_see_login_link_on_product_page(browser, screenshot_on_fail
 
 @pytest.mark.need_review
 @allure.step
-def test_guest_can_go_to_login_page_from_product_page(browser, screenshot_on_failure):
+def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
@@ -69,7 +69,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser, screenshot_on_fai
 
 @pytest.mark.need_review
 @allure.step
-def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, screenshot_on_failure):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
@@ -90,14 +90,14 @@ class TestUserAddToBasketFromProductPage():
         self.login_page.register_new_user(str(time.time()) + "@fakemail.org", str(time.time()))
 
     @allure.step
-    def test_user_cant_see_success_message(self, browser, screenshot_on_failure):
+    def test_user_cant_see_success_message(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
     @allure.step
-    def test_user_can_add_product_to_basket(self, browser, screenshot_on_failure):
+    def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
         page.add_to_busket()
