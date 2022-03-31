04f262bff3e9fe06208f744348a7fc3ad6420f56
PavlovaKsenya <snegurka64@mail.ru>
2022-04-01 00:12:41 +0400

    delete sys.path.append('../')

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,15 +1,11 @@
 import pytest
-import allure
-import sys
-sys.path.append('../')
-from pages.basket_page import BasketPage
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
+from ..pages.basket_page import BasketPage
+from ..pages.main_page import MainPage
+from ..pages.login_page import LoginPage
 
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
-    @allure.step
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com"
         page = MainPage(browser, link)
@@ -18,14 +14,12 @@ class TestLoginFromMainPage():
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
 
-    @allure.step
     def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
         page.open()
         page.should_be_login_link()
 
-@allure.step
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,11 +1,9 @@
 import time
 import allure
 import pytest
-import sys
-sys.path.append('../')
-from pages.basket_page import BasketPage
-from pages.login_page import LoginPage
-from pages.product_page import ProductPage
+from ..pages.basket_page import BasketPage
+from ..pages.login_page import LoginPage
+from ..pages.product_page import ProductPage
 
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -18,7 +16,6 @@ from pages.product_page import ProductPage
                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-@allure.step
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
@@ -28,15 +25,13 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.valid_price()
 
 @pytest.mark.xfail
-@allure.step
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, screenshot_on_failure):
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_busket()
     page.should_not_be_success_message()
 
-@allure.step
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
@@ -44,7 +39,6 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 # @pytest.mark.xfail
-@allure.step
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
@@ -52,7 +46,6 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.add_to_busket()
     page.should_disappeared_success_message()
 
-@allure.step
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -60,7 +53,6 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 @pytest.mark.need_review
-@allure.step
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -68,7 +60,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
 
 @pytest.mark.need_review
-@allure.step
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -89,14 +80,12 @@ class TestUserAddToBasketFromProductPage():
         self.login_page = LoginPage(browser, browser.current_url)
         self.login_page.register_new_user(str(time.time()) + "@fakemail.org", str(time.time()))
 
-    @allure.step
     def test_user_cant_see_success_message(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
         page.should_not_be_success_message()
 
     @pytest.mark.need_review
-    @allure.step
     def test_user_can_add_product_to_basket(self, browser):
         page = ProductPage(browser, self.link)
         page.open()
