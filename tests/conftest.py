13dad792e14cdb37e1c8ec64df7923edc90dabda
PavlovaKsenya <snegurka64@mail.ru>
2022-04-03 13:07:28 +0400

    add headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,7 @@ def browser(request):
     user_language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    options.add_argument("--headless")
     browser = webdriver.Chrome(options=options)
 
     yield browser
